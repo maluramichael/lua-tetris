afa5afd837b80195440698d3584b946c518297d5
Michael Malura <maluramichael@googlemail.com>
2017-08-08 19:16:05 +0200

    Resize game. Reset player posiotion everytime a form is placed.

--- main.lua
+++ main.lua
@@ -12,12 +12,12 @@ function love.load()
     image = love.graphics.newImage("assets/blocks.png")
     quad = love.graphics.newQuad(48, 80, 16, 16, image:getWidth(), image:getHeight())
 
-    tilesize = 128
+    tilesize = 64
 
     quadscale = tilesize / 16
-    padding = 0
-    mapwidth = 8
-    mapheight = 10
+    padding = 5
+    mapwidth = 10
+    mapheight = 23
     map = {}
     for y = 1, mapheight do
         map[y] = {}
@@ -27,27 +27,42 @@ function love.load()
     end
 
     forms = {}
-    forms.L = {{1, 0}, {1, 0}, {1, 1}}
-    forms.J = {{0, 1}, {0, 1}, {1, 1}}
-    forms.T = {{1, 1, 1}, {0, 1, 0}}
-    forms.O = {{1, 1}, {1, 1}}
-    forms.S = {{0, 1, 1}, {1, 1, 0}}
-    forms.Z = {{1, 1, 0}, {0, 1, 1}}
-    forms.I = {{1}, {1}, {1}, {1}}
-    forms.TEST = {{1}}
+    forms["L"] = {{1, 0}, {1, 0}, {1, 1}}
+    forms["J"] = {{0, 1}, {0, 1}, {1, 1}}
+    forms["T"] = {{1, 1, 1}, {0, 1, 0}}
+    forms["O"] = {{1, 1}, {1, 1}}
+    forms["S"] = {{0, 1, 1}, {1, 1, 0}}
+    forms["Z"] = {{1, 1, 0}, {0, 1, 1}}
+    forms["I"] = {{1}, {1}, {1}, {1}}
+    -- forms["TEST"] = {{1}}
 
-    start = {x=3, y = 0}
+    start = {x = 3, y = 0}
+
+    math.randomseed(os.time())
 
     player = {
         x = start.x,
         y = start.y,
-        form = forms.T
+        form = randomelement(forms),
+        next = randomelement(forms)
     }
+
+    --setform(0, 6) -- just for debug purpose
+    resetplayer()
 
     fallspeed = 1
     tick = fallspeed
 
-    setform(0, 6)
+end
+
+function randomelement(elements)
+    -- iterate over whole table to get all keys
+    local keyset = {}
+    for k in pairs(elements) do
+        table.insert(keyset, k)
+    end
+    -- now you can reliably return a random key
+    return elements[keyset[math.random(#keyset)]]
 end
 
 function love.keypressed(key, scancode, isrepeat)
@@ -91,6 +106,7 @@ function formmovable(dx, dy)
     formwidth = #player.form[1]
     formheight = #player.form
 
+    print(dx, dy)
     -- form goes down
     if dy == 1 then
         -- +1 because player.x and y starts with 0 and lua tables start with 1
@@ -103,32 +119,39 @@ function formmovable(dx, dy)
                 blockbelowx = playerxinmap + (x - 1)
                 blockbelowy = playeryinmap + (y - 1) + 1
 
+                if blockbelowy > mapheight then
+                    setform(player.x, player.y)
+                    resetplayer()
+                    return false
+                end
+
                 blockbelow = map[blockbelowy][blockbelowx]
 
                 -- check if anything is beneath the block
                 if player.form[y][x] == 1 and blockbelow == 1 then
                     setform(player.x, player.y)
-                    player.x = start.x
-                    player.y = start.y
+                    resetplayer()
                     return false
                 end
             end
         end
-
-        if (player.y + formheight) < mapheight then
-            return true
-        end
     end
 
-    if dx == -1 and (player.x) > 0 then
-        return true
+    if dx == -1 and (player.x) <= 0 then
+        return false
     end
 
-    if dx == 1 and (player.x + formwidth) < mapwidth then
-        return true
+    if dx == 1 and (player.x + formwidth) > mapwidth - 1 then
+        return false
     end
 
-    return false
+    return true
+end
+
+function resetplayer()
+    player.x = start.x
+    player.y = start.y
+    player.form = randomelement(forms)
 end
 
 function setform(xoffset, yoffset)
